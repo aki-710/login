ed9fc32ea8376b465887d5e63cfb0e8269394718
aki <akidesugananika2022@gmail.com>
2026-04-12 21:12:28 +0900

    password が露出してたので修正

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
-// db.js の正しい冒頭
-require('dotenv').config(); // これが一番上でないと process.env が空になる
+require('dotenv').config(); // 環境変数の読み込み（最優先）
 const { Pool } = require('pg');
 
-console.log("@bZp%*DSxE&sBeXw1#JX", process.env.DB_PASSWORD); // ここで確認
-
+// データベース接続の設定
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+});
+
+// 接続テスト用（エラーが起きた場合にログを出す設定）
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 module.exports = {
